062276ea8266a5d2b3d4e27b94604f5691c55c3f
Alvin CHENG <ccheng93@tux-169.cae.wisc.edu>
2024-06-04 18:22:57 -0500

    Add lab10, learn callback

---
+++ uvm_training_lab/labs/lab10/my_sequence.sv
@@ -0,0 +1,34 @@
+class my_sequence extends uvm_sequence #(my_transaction);
+
+    `uvm_object_utils(my_sequence)
+
+    int item_num = 10;
+    function new(string name = "my_sequence");
+        super.new(name);
+    endfunction
+
+    function void pre_randomize();
+        uvm_config_db#(int)::get(m_sequencer, "", "item_num", item_num);
+    endfunction
+
+    virtual task body();
+        my_transaction tr;
+        if(starting_phase != null)
+            starting_phase.raise_objection(this);
+
+        repeat(item_num) begin
+            // Equivalent to `uvm_do(req)
+            tr = my_transaction::type_id::create("tr");
+            start_item(tr);
+            tr.randomize();
+            finish_item(tr);
+
+            get_response(rsp);
+            `uvm_info("SEQ", {"\n", "Sequence get the resposne: \n", rsp.sprint()}, UVM_MEDIUM)
+        end
+
+        #100;
+        if(starting_phase != null)
+            starting_phase.drop_objection(this);
+    endtask
+endclass
