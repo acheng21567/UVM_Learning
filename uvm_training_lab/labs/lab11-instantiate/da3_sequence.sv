a544626219964b4b22a1ed265f698cdeeda0b501
Alvin CHENG <ccheng93@tux-169.cae.wisc.edu>
2024-06-04 21:00:54 -0500

    Add lab11, learn sequence library and set variables

---
+++ uvm_training_lab/labs/lab11-instantiate/da3_sequence.sv
@@ -0,0 +1,37 @@
+class da3_sequence extends my_sequence;
+
+    `uvm_object_utils(da3_sequence)
+
+    // Add to sequence library permanently
+    // `uvm_add_to_seq_lib(da3_sequence, my_sequence_lib)
+
+    int item_num = 10;
+    function new(string name = "da3_sequence");
+        super.new(name);
+    endfunction
+
+    function void pre_randomize();
+        uvm_config_db#(int)::get(m_sequencer, "", "item_num", item_num);
+    endfunction
+
+    virtual task body();
+        my_transaction tr;
+        if(starting_phase != null)
+            starting_phase.raise_objection(this);
+
+        repeat(item_num) begin
+            // Equivalent to `uvm_do(req)
+            tr = my_transaction::type_id::create("tr");
+            start_item(tr);
+            tr.randomize() with {tr.da == 3;};
+            finish_item(tr);
+
+            get_response(rsp);
+            `uvm_info("DA3_SEQ", {"\n", "Sequence get the resposne: \n", rsp.sprint()}, UVM_MEDIUM)
+        end
+
+        #100;
+        if(starting_phase != null)
+            starting_phase.drop_objection(this);
+    endtask
+endclass
---
+++ uvm_training_lab/labs/lab11-perm/sa6_sequence.sv
@@ -0,0 +1,37 @@
+class sa6_sequence extends my_sequence;
+
+    `uvm_object_utils(sa6_sequence)
+    
+    // Add to sequence library permanently
+    `uvm_add_to_seq_lib(sa6_sequence, my_sequence_lib)
+
+    int item_num = 10;
+    function new(string name = "sa3_sequence");
+        super.new(name);
+    endfunction
+
+    function void pre_randomize();
+        uvm_config_db#(int)::get(m_sequencer, "", "item_num", item_num);
+    endfunction
+
+    virtual task body();
+        my_transaction tr;
+        if(starting_phase != null)
+            starting_phase.raise_objection(this);
+
+        repeat(item_num) begin
+            // Equivalent to `uvm_do(req)
+            tr = my_transaction::type_id::create("tr");
+            start_item(tr);
+            tr.randomize() with {tr.sa == 6;};
+            finish_item(tr);
+
+            get_response(rsp);
+            `uvm_info("SA6_SEQ", {"\n", "Sequence get the resposne: \n", rsp.sprint()}, UVM_MEDIUM)
+        end
+
+        #100;
+        if(starting_phase != null)
+            starting_phase.drop_objection(this);
+    endtask
+endclass
